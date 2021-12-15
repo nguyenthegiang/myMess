6986c2b3ce666d150ca9c0d085585aeddefca3ba
nguyenthegiang <giangnthe153046@fpt.edu.vn>
2021-12-15 11:09:04 +0700

    Copy from Shop-App: main.dart & Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import './screens/auth_screen.dart';
+import './providers/auth.dart';
+import './screens/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,12 +14,36 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MyMess',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
+    return MultiProvider(
+      providers: [
+        /* Provider cho auth */
+        ChangeNotifierProvider(
+          create: (ctx) => Auth(),
+        ),
+      ],
+      child: Consumer<Auth>(
+        builder: (ctx, auth, _) => MaterialApp(
+          title: 'MyMess',
+          theme: ThemeData(
+            primarySwatch: Colors.green,
+            fontFamily: 'Lato',
+          ),
+          home: auth.isAuth
+              ? Scaffold(
+                  body: Container(
+                    child: Text('hi'),
+                  ),
+                )
+              : FutureBuilder(
+                  future: auth.tryAutoLogin(),
+                  builder: (ctx, authResultSnapshot) =>
+                      authResultSnapshot.connectionState ==
+                              ConnectionState.waiting
+                          ? SplashScreen()
+                          : AuthScreen(),
+                ),
+        ),
       ),
-      home: AuthScreen(),
     );
   }
 }
