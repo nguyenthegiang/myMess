25ebff6ab511342f56e83dfecc176475a5c45142
nguyenthegiang <giangnthe153046@fpt.edu.vn>
2021-12-17 09:15:11 +0700

    Home Screen - Back-End: Pull to Refresh

--- lib/providers/message_provider.dart
+++ lib/providers/message_provider.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 //import cái này để gửi http request
 import 'package:http/http.dart' as http;
 
-import '../models/http_exception.dart';
+import '../models/message.dart';
 
 class MessageProvider with ChangeNotifier {
   //String để lưu giữ Token khi Authentication
@@ -65,6 +65,68 @@ class MessageProvider with ChangeNotifier {
           'timeStamp': DateTime.now().toIso8601String(),
         }),
       );
+    } catch (error) {
+      print(error);
+      rethrow;
+    }
+  }
+
+  /* Function để lấy những list người đã nhắn tin với mình -> Hiển thị List Message */
+  Future<List<String>> getFriends() async {
+    //list lưu những message tới mình hoặc mình gửi đi -> từ đó lọc ra friend
+    List<Message> listMess = [];
+
+    //Lấy những Message có mình là sender
+    listMess.addAll(await getMessages(isSender: true));
+
+    //Lấy những Message có mình là receiver
+    listMess.addAll(await getMessages(isSender: false));
+
+    //list những ng đã nhắn tin vs mình
+    List<String> friendList = [];
+
+    print(listMess[1]);
+
+    return friendList;
+  }
+
+  /* function để lấy những message có mình là sender hoặc receiver từ server */
+  Future<List<Message>> getMessages({required bool isSender}) async {
+    //biến để lưu list message
+    List<Message> listMess = [];
+
+    //biến để gán vào filterString tùy theo muốn tìm theo sender hay receiver
+    String role = isSender ? 'senderID' : 'receiverID';
+
+    final filterString = 'orderBy="' + role + '"&equalTo="$userId"';
+    final url =
+        'https://my-mess-39d32-default-rtdb.firebaseio.com/message.json?auth=$authToken&$filterString';
+    try {
+      final response = await http.get(
+        Uri.parse(url),
+      );
+
+      //decode data
+      final extractedData = json.decode(response.body) as Map<String, dynamic>?;
+
+      //null thì return luôn
+      if (extractedData == null) {
+        return listMess;
+      }
+
+      //gán vào list message
+      extractedData.forEach((id, data) {
+        listMess.add(
+          Message(
+            messageContent: data['messageContent'],
+            senderId: data['senderID'],
+            receiverId: data['receiverID'],
+            timeStamp: DateTime.parse(data['timeStamp']),
+          ),
+        );
+      });
+
+      return listMess;
     } catch (error) {
       print(error);
       rethrow;
--- lib/screens/home-screen.dart
+++ lib/screens/home-screen.dart
@@ -5,6 +5,7 @@ import 'package:provider/provider.dart';
 import '../providers/auth.dart';
 import '../widgets/app_drawer.dart';
 import '../screens/new_message_screen.dart';
+import '../providers/message_provider.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -16,29 +17,13 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   //Biến để cho didChangeDependencies() chỉ chạy 1 lần thôi
   var _isInit = true;
-  //biến để làm loading spinner
-  var _isLoading = false;
 
-  @override
-  void didChangeDependencies() {
-    if (_isInit) {
-      setState(() {
-        //chuyển màn hình sang loading
-        _isLoading = true;
-      });
-
-      /* Khi mới vào home screen thì load list message*/
-      // Provider.of<Products>(context).fetchAndSetProducts().then((_) {
-      //   setState(() {
-      //     //chuyển màn hình lại bình thường sau khi lấy data xong
-      //     _isLoading = false;
-      //   });
-      // });
-
-      _isLoading = false;
-    }
-    _isInit = false;
-    super.didChangeDependencies();
+  //function để refresh lại list friends
+  Future<void> _refreshFriendList(BuildContext context) async {
+    await Provider.of<MessageProvider>(
+      context,
+      listen: false,
+    ).getFriends();
   }
 
   @override
@@ -47,78 +32,92 @@ class _HomeScreenState extends State<HomeScreen> {
     final username = Provider.of<Auth>(context).username;
 
     return Scaffold(
-        appBar: AppBar(
-          title: Text('MyMess - $username'),
-          actions: [
-            //Nhấn nút này để chuyển sang NewMessageScreen
-            IconButton(
-              onPressed: () {
-                Navigator.of(context).pushNamed(NewMessageScreen.routeName);
-              },
-              icon: const Icon(Icons.edit_sharp),
-              tooltip: 'Tạo tin nhắn mới',
-            ),
-          ],
-        ),
-        drawer: AppDrawer(),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: ListView(
-            children: [
-              //Searchbox - Not implemented yet
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 10,
-                  vertical: 8,
-                ),
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(50)),
-                    ),
-                    hintText: 'Tìm kiếm',
-                    prefixIcon: Icon(Icons.search),
+      appBar: AppBar(
+        title: Text('MyMess - $username'),
+        actions: [
+          //Nhấn nút này để chuyển sang NewMessageScreen
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).pushNamed(NewMessageScreen.routeName);
+            },
+            icon: const Icon(Icons.edit_sharp),
+            tooltip: 'Tạo tin nhắn mới',
+          ),
+        ],
+      ),
+      drawer: AppDrawer(),
+      body: FutureBuilder(
+        //tính năng pull to refresh
+        future: _refreshFriendList(context),
+        builder: (ctx, snapshot) => snapshot.connectionState ==
+                ConnectionState.waiting
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : RefreshIndicator(
+                onRefresh: () => _refreshFriendList(context),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListView(
+                    children: [
+                      //Searchbox - Not implemented yet
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 10,
+                          vertical: 8,
+                        ),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(50)),
+                            ),
+                            hintText: 'Tìm kiếm (Tính năng này chưa hỗ trợ)',
+                            prefixIcon: Icon(Icons.search),
+                          ),
+                        ),
+                      ),
+
+                      /*Message List - sau này chuyển thành ListView.builder và 
+                      ListTile tách ra Widget riêng*/
+                      Container(
+                        height: MediaQuery.of(context).size.height -
+                            MediaQuery.of(context).padding.top -
+                            150,
+                        child: ListView(
+                          children: [
+                            Card(
+                              elevation: 5,
+                              child: ListTile(
+                                title: Text('Nguyen The Giang'),
+                                subtitle: Text('Message sent'),
+                                leading: CircleAvatar(
+                                  backgroundImage: AssetImage(
+                                      'assets/images/default-avatar.png'),
+                                ),
+                                trailing: Icon(Icons.check_circle),
+                              ),
+                            ),
+                            Card(
+                              elevation: 5,
+                              child: ListTile(
+                                title: Text('Nguyen The Giang'),
+                                subtitle: Text('Message sent'),
+                                leading: CircleAvatar(
+                                  backgroundImage: AssetImage(
+                                      'assets/images/default-avatar.png'),
+                                ),
+                                trailing: Icon(Icons.check_circle),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ),
-
-              /*Message List - sau này chuyển thành ListView.builder và 
-              ListTile tách ra Widget riêng*/
-              Container(
-                height: MediaQuery.of(context).size.height -
-                    MediaQuery.of(context).padding.top -
-                    150,
-                child: ListView(
-                  children: [
-                    Card(
-                      elevation: 5,
-                      child: ListTile(
-                        title: Text('Nguyen The Giang'),
-                        subtitle: Text('Message sent'),
-                        leading: CircleAvatar(
-                          backgroundImage:
-                              AssetImage('assets/images/default-avatar.png'),
-                        ),
-                        trailing: Icon(Icons.check_circle),
-                      ),
-                    ),
-                    Card(
-                      elevation: 5,
-                      child: ListTile(
-                        title: Text('Nguyen The Giang'),
-                        subtitle: Text('Message sent'),
-                        leading: CircleAvatar(
-                          backgroundImage:
-                              AssetImage('assets/images/default-avatar.png'),
-                        ),
-                        trailing: Icon(Icons.check_circle),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ));
+      ),
+    );
   }
 }
